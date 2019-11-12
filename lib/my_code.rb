4effa92617c108cc59b44c1394befb1ccb6503ee
Yu Hao Ye <rxye1991@gmail.com>
2019-11-12 01:31:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,10 @@
 # Your Code Here
+# own implentation of map 
+def map(array)
+  new = []
+  i = 0 
+  while i < array.length
+  # yield pass element of the array to the block. map{ block }
+    new.push(yield(array[i]))
+    i += 1 
+end
