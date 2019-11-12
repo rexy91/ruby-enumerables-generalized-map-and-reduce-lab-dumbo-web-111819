80b6f1a76a80325e04ba90d27b1ccbc00fd18301
Yu Hao Ye <rxye1991@gmail.com>
2019-11-12 01:49:10 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,28 @@
 # Your Code Here
+# own implentation of map 
+def map(array)
+  new = []
+  i = 0 
+  while i < array.length
+  # yield pass element of the array to the block. map{ block }
+    new.push(yield(array[i]))
+    i += 1 
+  end
+  new 
+end
+
+def reduce(array, sv=nil)
+  if sv
+    sum = sv
+    i=0
+  else
+    sum = array[0]
+    i = 1 
+  end
+  while i < array.length
+    sum = yield(sum, array[i])
+    i += 1 
+  end
+  sum
+end
+  
