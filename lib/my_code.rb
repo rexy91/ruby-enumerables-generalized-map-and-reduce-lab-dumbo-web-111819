207194a2a3ad93be51ac07489f6f0321f03778d0
Yu Hao Ye <rxye1991@gmail.com>
2019-11-12 01:33:17 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -5,7 +5,7 @@ def map(array)
   i = 0 
   while i < array.length
   # yield pass element of the array to the block. map{ block }
-    new.push(yield(array[i] * -1))
+    new.push(yield(array[i]))
     i += 1 
   end
   new 
